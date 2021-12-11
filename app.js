71c05879a4c930ed207a40c18f61052e15e9081d
TiziBasso <bassodigitales@gmail.com>
2021-12-11 01:45:09 -0300

    condicionales

---
+++ app.js
@@ -0,0 +1,226 @@
+// Condicionales 
+
+/*Ejercicio 1 
+
+let medidaEnMetros = prompt ("Ingrese su medida en Metros ");
+let comprobacion = Number.isFinite(parseFloat(medidaEnMetros)); // para todo numero finito (Number.isFinite)
+
+if(comprobacion == true){
+let medidaEnPies = parseFloat (medidaEnMetros) * 3.28084 ;
+let medidaEnPulgadas = parseFloat (medidaEnMetros) * 39.3701 ;
+let medidaEnCentimetros = parseFloat (medidaEnMetros) * 100 ;
+    alert ("Pies= " + medidaEnPies.toFixed (2) + ("ft") + "\nPulgadas= " + medidaEnPulgadas.toFixed (2) + ("in") + "\nCentímetros= " + medidaEnCentimetros.toFixed ("2") + ("cm"));
+}else {
+    alert("Error NaN");
+}
+
+*/
+
+/*ejercicio 2
+
+let edad = prompt ("Ingrese su edad ");
+
+if(edad < 18){
+    document.write("eres menor de edad");
+}else {
+    document.write("eres mayor de edad");
+}
+
+*/
+
+/* Ejercicio 3
+
+let sabores = prompt("Digame sus sabores");
+let confirmacion = prompt("¿quiere cobertura de chocolate?")
+
+if(confirmacion.toLowerCase() == "si"){
+    alert("Su helado sabor " + sabores + " con cobertura de chocolate cuesta $180");
+}else {
+    alert("Su helado sabor " + sabores + " sin cobertura de chocolate cuesta $150");
+}
+
+*/
+
+/* Ejercicio  4
+
+let numero1 = prompt ("Ingrese el primer número");
+let numero2 = prompt ("Ingrese el segundo número");
+let resultado = parseFloat (numero1) + parseFloat (numero2);
+console.log("El resultado de la suma es: " + resultado);
+
+resultado /= 2; // resultado = resultado / 2;
+
+if(parseInt(resultado) == resultado) {
+    console.log("El numero es par");
+}else {
+    console.log("El numero es impar");
+}
+
+*/
+
+/* Ejercicio 5
+
+let numero = prompt ("Ingrese un número");
+
+if(numero < 0){
+    document.write("el numero es negativo");
+}else if(numero > 0){
+    document.write("el numero es positivo");
+}else {
+    document.write("el numero es 0");
+}
+
+*/
+
+/* Ejercicio 6
+
+let monto = prompt("Ingrese cantidad de dinero");
+
+if(monto > 2000){
+    let descuento = monto - monto*0.1;
+    document.write("el total a pagar es " + descuento);
+}else {
+    document.write("total a pagar " + monto);
+}
+
+*/
+
+/*ejercicio 7 
+
+ let texto = prompt("ingrese un texto");
+ let posicion = prompt("ingrese un numero de posicion");
+
+ let letra = texto.slice(posicion-1,posicion);
+
+ letra = letra.toLowerCase();
+
+ if(letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"){
+    document.write("la letra seleccionada es vocal");
+ }else {
+    document.write("la letra no es una vocal");
+ }
+
+*/
+
+/*ejercicio 8
+
+let nota = prompt("ingrese su nota del 1 al 10");
+
+if(nota <= 3){
+    document.write("aplazado");
+}else if(nota >= 4 && nota <=5){
+    document.write("reprobado");
+}else{
+    document.write("aprobado");
+}
+
+*/
+
+/*ejercicio 9
+
+let numero1 = prompt("ingrese primer numero");
+let numero2 = prompt("ingrese segundo numero");
+
+if(numero1 > numero2){
+    document.write("el primer numero es mayor");
+}else if(numero1 == numero2){
+    document.write("los numeros son iguales");
+}else {
+    document.write("el segundo numero es mayor");
+}
+
+*/
+
+/*ejercicio 10
+
+let mes = prompt("ingrese un mes del 1 al 12");
+document.write(new Date(2021, mes, 0).getDate());
+//new date sirve para ingresar una nueva fecha (num_año , num_mes , num_dia)
+// Le agregamos un 0 en el numero de dia para que vaya al ultimo dia del mes anterior. Ya que el mes lo toma desde el 0 al 11 siendo 0 enero y 11 diciembre
+
+*/
+
+/*ejercicio 11
+
+let numero1 = prompt("ingrese primer numero");
+let numero2 = prompt("ingrese segundo numero");
+let numero3 = prompt("ingrese tercer numero");
+
+if(numero1 > numero2 && numero1 > numero3){
+    document.write("el primer numero es el mayor");
+}else if(numero2 > numero1 && numero2 > numero3){
+    document.write("el segundo numero es el mayor");
+}else if(numero3 > numero1 && numero3 > numero2){
+    document.write("el tercer numero es el mayor");
+}else {
+    document.write("Hay numeros iguales");
+}
+
+*/
+
+/*ejercicio 12
+
+let  edad = prompt("ingrese su edad");
+let bebida = prompt("ingrese una bebida entre: Cerveza $50, jugo $10, agua $1");
+
+alert("NO SE FIA, NO INGRESAR NUMEROS NEGATIVOS");
+
+
+if(bebida.toLowerCase() == "cerveza" && edad < 18){
+    alert("Prohibida su venta a menores de edad (Excepto en argentina)");
+}else {
+    let monto = prompt("¿con cuanto dinero pagara?");
+    
+    if(bebida.toLowerCase() == "cerveza"){
+        monto -= 50;
+        document.write("su vuelto sera " + monto);
+    }else if (bebida.toLowerCase() == "jugo"){
+        monto -= 10;
+        document.write("su vuelto sera " + monto);
+    }else if(bebida.toLowerCase() == "agua"){
+        monto -= 1;
+        document.write("su vuelto sera " + monto);
+    }
+}
+*/
+
+/*ejercicio 13
+
+let mes = prompt("Ingrese un mes del 1 al 12");
+if (mes == 1) {
+    alert("Enero");
+}else if (mes == 2) {
+    alert("Febrero");
+}else if (mes == 3) {
+    alert("Marzo");
+}else if (mes == 4) {
+    alert("Abril");
+}else if (mes == 5) {
+    alert("Mayo");
+}else if (mes == 6) {
+    alert("Junio");
+}else if (mes == 7) {
+    alert("Julio");
+}else if (mes == 8) {
+    alert("Agosto");
+}else if (mes == 9) {
+    alert("Septiembre");
+}else if (mes == 10) {
+    alert("Octubre");
+}else if (mes == 11) {
+    alert("Noviembre");
+}else if (mes == 12) {
+    alert("Diciembre");
+}
+
+*/
+
+/*BONUS LOS HACE LUCIA Y SIN AYUDA :) :)
+
+1. Solicitar al usuario su clave de 4 dígitos (que tendremos almacenada en una variable) y el monto a retirar. Si la clave es correcta y el monto mayor al balance, realizar la operación. Sino, mostrar un mensaje de error en caso de ser la clave incorrecta o pedir más dinero del disponible.
+
+2. Solicitar al usuario su día y mes de nacimiento e indicar cual es su signo zodiacal.
+
+3. Solicitar al usuario que ingrese la hora y su nombre. Si son entre las 12 inclusive y 19 saludar con buenas tardes, entre 19 inclusive y 5 buenas noches, y entre 5 inclusive y 12 buenos días. 
+
+*/
